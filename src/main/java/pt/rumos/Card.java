525ece77950efb3cf6fcd044f5aed8ff2f74878e
miguelsilva4100 <miguelsilva4100@gmail.com>
2019-12-15 15:35:02 +0000

    Card class created

---
+++ src/main/java/pt/rumos/Card.java
@@ -0,0 +1,16 @@
+package pt.rumos;
+
+public class Card {
+
+    private int cardNumber;
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+
+}
